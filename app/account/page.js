c6f5bda7e1c0a7f96526b29d440dca2a3f3a1b5e
dejmedus <email96123@gmail.com>
2023-04-21 23:08:49 -0600

    prtoect account route & add signout button

--- app/account/page.js
+++ app/account/page.js
@@ -1,20 +1,40 @@
 'use client'
 
-import { useEffect } from "react";
 import { useAuthContext } from "@/context/AuthContext";
+import signOutUser from "@/firebase/signout";
 import { useRouter } from "next/navigation";
 
 function Account() {
     const { user } = useAuthContext()
     const router = useRouter()
 
-    useEffect(() => {
-        if (user == null) router.push("/signup")
-    }, [user])
+    async function logout() {
+        const { result, error } = await signOutUser();
 
-    return (<>
-        <h1>You are logged in as {user.email}</h1>
-    </>);
+        if (error) {
+            return console.log(error)
+        }
+
+        // else successful
+        console.log(result)
+        return router.push("/")
+    }
+
+    if (user) {
+        return (<>
+            <h1>You are logged in as {user.email}</h1>
+            <button
+                className="block rounded-md bg-lime-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-lime-700"
+                onClick={logout}
+            >
+                Signout
+            </button>
+        </>);
+    }
+    else {
+        router.push("/signin")
+    }
+
 }
 
 export default Account;
