bc36c621fb1316706aa14eab2ed4d6d454f18def
dejmedus <email96123@gmail.com>
2023-04-23 00:01:52 -0600

    add get data from firestore

---
+++ db/getData.js
@@ -0,0 +1,20 @@
+import firebase_app from "@/firebase/config";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+const db = getFirestore(firebase_app)
+
+export default async function getData(collection, id) {
+
+    let docRef = doc(db, collection, id);
+
+    let result = null;
+    let error = null;
+
+    try {
+        result = await getDoc(docRef);
+
+    } catch (e) {
+        error = e
+    }
+
+    return { result, error };
+}
