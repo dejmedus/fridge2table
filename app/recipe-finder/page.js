e88f2c6ab4dd94c0e7cd15daddcfc596aa9c6bb6
dejmedus <email96123@gmail.com>
2023-04-23 00:04:27 -0600

    add recipes to cookbook

--- app/recipe-finder/page.js
+++ app/recipe-finder/page.js
@@ -1,17 +1,20 @@
 'use client'
 
-// import { useAuthContext } from "@/context/AuthContext";
+import { useAuthContext } from "@/context/AuthContext";
 import { useEffect, useState } from 'react'
 import Form from '@/components/Form'
+import getData from "@/db/getData";
+import { RecipeCard } from "@/components/RecipeCard";
 
 export default function RecipeFinder() {
-    // const { user } = useAuthContext();
+    const { user } = useAuthContext();
     const [recipes, setRecipes] = useState({});
     const [ingredients, setIngredients] = useState([]);
     const [topRecipes, setTopRecipes] = useState([]);
+    const [cookbook, setCookbook] = useState([]);
 
     useEffect(() => {
-        async function getData() {
+        async function getRecipeData() {
             const res = await fetch('/api/recipeData');
 
             if (!res.ok) {
@@ -21,7 +24,22 @@ export default function RecipeFinder() {
             const recipeData = await res.json();
             setRecipes(recipeData);
         }
-        getData();
+        async function getCookbook() {
+            const { result, error } = await getData('users', user.uid)
+
+            if (error) {
+                return console.log(error)
+            } else {
+                const userData = result.data()
+
+                setCookbook(userData.cookbook)
+            }
+        }
+
+        getRecipeData();
+        if (user) {
+            getCookbook();
+        }
     }, [])
 
     return (<>
@@ -30,44 +48,17 @@ export default function RecipeFinder() {
         {topRecipes !== null ?
             topRecipes.map(topRecipe => {
                 const { id, href, title, ingredients, instructions } = topRecipe[1];
+                let inCookbook = false;
+
+                if (user) {
+                    inCookbook = cookbook.some(a => a.id === topRecipe[1].id);
+                }
+
+
                 return (
-                    <RecipeCard id={id} href={href} title={title} ingredients={ingredients} instructions={instructions} />
+                    <RecipeCard recipe={topRecipe[1]} id={id} href={href} title={title} ingredients={ingredients} instructions={instructions} inCookbook={inCookbook} cookbook={cookbook} setCookbook={setCookbook} user={user} />
                 )
             }) : null}
     </>);
 }
 
-const RecipeCard = ({ id, href, title, ingredients, instructions }) => {
-    return (<div
-        key={id}
-        className="relative block p-4 mt-4 overflow-hidden bg-white border rounded-lg border-neutral-100 sm:p-6 lg:p-8"
-    >
-        <span
-            className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-lime-700 via-lime-800 to-lime-900"
-        ></span>
-
-        <div className="sm:flex sm:gap-4">
-            <div>
-                <a href={href} className="text-lg font-bold hover:underline text-neutral-900 sm:text-xl">
-                    {title}
-                </a>
-            </div>
-        </div>
-
-        <div className="flex flex-col gap-1 mt-4">
-            <p className="mt-1 text-xs font-medium text-neutral-600">Ingredients</p>
-            <p className="max-w-[80ch] text-sm text-neutral-500">
-                {ingredients}
-            </p>
-        </div>
-
-        <div className="flex flex-col gap-1 mt-4">
-            <p className="mt-1 text-xs font-medium text-neutral-600">Instructions</p>
-            <p className="max-w-[80ch] text-sm text-neutral-500">
-                {instructions}
-            </p>
-        </div>
-
-    </div>
-    )
-}
